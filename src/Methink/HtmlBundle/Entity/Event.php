bb75583fe9d25755b76e04d842135132c0279e97
Mino Wu <minowu@foxmail.com>
2013-07-03 17:04:48 +0800

    新建Event和User实体

---
+++ src/Methink/HtmlBundle/Entity/Event.php
@@ -0,0 +1,328 @@
+<?php
+
+namespace Methink\HtmlBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * Event
+ *
+ * @ORM\Table()
+ * @ORM\Entity
+ */
+class Event
+{
+    /**
+     * @var integer
+     *
+     * @ORM\Column(name="id", type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    private $id;
+
+    /**
+     * @var integer
+     *
+     * @ORM\ManyToOne(targetEntity="User", inversedBy="event")
+     * @ORM\JoinColumn(name="user_id", referencedColumnName="id")
+     */
+    private $user;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="title", type="string", length=255)
+     */
+    private $title;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="content", type="string", length=500)
+     */
+    private $content;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="address", type="string", length=255)
+     */
+    private $address;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="organizer", type="string", length=255)
+     */
+    private $organizer;
+
+    /**
+     * @var \DateTime
+     *
+     * @ORM\Column(name="start_at", type="datetime")
+     */
+    private $startAt;
+
+    /**
+     * @var \DateTime
+     *
+     * @ORM\Column(name="end_at", type="datetime")
+     */
+    private $endAt;
+
+    /**
+     * @var integer
+     *
+     * @ORM\Column(name="hidden", type="smallint")
+     */
+    private $hidden;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="coverpath", type="string", length=255)
+     */
+    private $coverpath;
+
+
+    /**
+     * Get id
+     *
+     * @return integer 
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set title
+     *
+     * @param string $title
+     * @return Event
+     */
+    public function setTitle($title)
+    {
+        $this->title = $title;
+    
+        return $this;
+    }
+
+    /**
+     * Get title
+     *
+     * @return string 
+     */
+    public function getTitle()
+    {
+        return $this->title;
+    }
+
+    /**
+     * Set content
+     *
+     * @param string $content
+     * @return Event
+     */
+    public function setContent($content)
+    {
+        $this->content = $content;
+    
+        return $this;
+    }
+
+    /**
+     * Get content
+     *
+     * @return string 
+     */
+    public function getContent()
+    {
+        return $this->content;
+    }
+
+    /**
+     * Set address
+     *
+     * @param string $address
+     * @return Event
+     */
+    public function setAddress($address)
+    {
+        $this->address = $address;
+    
+        return $this;
+    }
+
+    /**
+     * Get address
+     *
+     * @return string 
+     */
+    public function getAddress()
+    {
+        return $this->address;
+    }
+
+    /**
+     * Set organizer
+     *
+     * @param string $organizer
+     * @return Event
+     */
+    public function setOrganizer($organizer)
+    {
+        $this->organizer = $organizer;
+    
+        return $this;
+    }
+
+    /**
+     * Get organizer
+     *
+     * @return string 
+     */
+    public function getOrganizer()
+    {
+        return $this->organizer;
+    }
+
+    /**
+     * Set startAt
+     *
+     * @param \DateTime $startAt
+     * @return Event
+     */
+    public function setStartAt($startAt)
+    {
+        $this->startAt = $startAt;
+    
+        return $this;
+    }
+
+    /**
+     * Get startAt
+     *
+     * @return \DateTime 
+     */
+    public function getStartAt()
+    {
+        return $this->startAt;
+    }
+
+    /**
+     * Set endAt
+     *
+     * @param \DateTime $endAt
+     * @return Event
+     */
+    public function setEndAt($endAt)
+    {
+        $this->endAt = $endAt;
+    
+        return $this;
+    }
+
+    /**
+     * Get endAt
+     *
+     * @return \DateTime 
+     */
+    public function getEndAt()
+    {
+        return $this->endAt;
+    }
+
+    /**
+     * Set hidden
+     *
+     * @param integer $hidden
+     * @return Event
+     */
+    public function setHidden($hidden)
+    {
+        $this->hidden = $hidden;
+    
+        return $this;
+    }
+
+    /**
+     * Get hidden
+     *
+     * @return integer 
+     */
+    public function getHidden()
+    {
+        return $this->hidden;
+    }
+
+    /**
+     * Set coverpath
+     *
+     * @param string $coverpath
+     * @return Event
+     */
+    public function setCoverpath($coverpath)
+    {
+        $this->coverpath = $coverpath;
+    
+        return $this;
+    }
+
+    /**
+     * Get coverpath
+     *
+     * @return string 
+     */
+    public function getCoverpath()
+    {
+        return $this->coverpath;
+    }
+
+    /**
+     * Set userId
+     *
+     * @param integer $userId
+     * @return Event
+     */
+    public function setUserId($userId)
+    {
+        $this->userId = $userId;
+    
+        return $this;
+    }
+
+    /**
+     * Get userId
+     *
+     * @return integer 
+     */
+    public function getUserId()
+    {
+        return $this->userId;
+    }
+
+    /**
+     * Set user
+     *
+     * @param \Methink\HtmlBundle\Entity\User $user
+     * @return Event
+     */
+    public function setUser(\Methink\HtmlBundle\Entity\User $user = null)
+    {
+        $this->user = $user;
+    
+        return $this;
+    }
+
+    /**
+     * Get user
+     *
+     * @return \Methink\HtmlBundle\Entity\User 
+     */
+    public function getUser()
+    {
+        return $this->user;
+    }
+}
