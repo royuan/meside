1c2b91c46d3da5626cf9c52e9c7ed0056249c5c2
Mino Wu <minowu@foxmail.com>
2013-07-03 17:05:10 +0800

    编辑EventController，及相关的路由和模板

--- src/Methink/HtmlBundle/Controller/EventController.php
+++ src/Methink/HtmlBundle/Controller/EventController.php
@@ -3,12 +3,21 @@
 namespace Methink\HtmlBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Response;
+use Methink\HtmlBundle\Entity\Event;
 
 class EventController extends Controller
 {
     public function indexAction()
     {
-    	// list
+    	$event = $this->getDoctrine()
+    	        ->getRepository('MethinkHtmlBundle:Event')
+    	        ->findAll();
+
+    	return $this->render(
+    		'MethinkHtmlBundle:Event:index.html.twig',
+    		array('datas' => $event)
+    	);
     }
 
     public function createAction()
