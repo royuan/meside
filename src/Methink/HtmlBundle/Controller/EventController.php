fe246874caac9baf98821beb651c2cc41e050ebb
Mino Wu <minowu@foxmail.com>
2013-07-04 16:19:45 +0800

    测试 Event form

--- src/Methink/HtmlBundle/Controller/EventController.php
+++ src/Methink/HtmlBundle/Controller/EventController.php
@@ -5,6 +5,7 @@ namespace Methink\HtmlBundle\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Response;
 use Methink\HtmlBundle\Entity\Event;
+use \DateTime;
 
 class EventController extends Controller
 {
@@ -22,6 +23,23 @@ class EventController extends Controller
 
     public function createAction()
     {
-    	// create
+		$event = new Event();
+		$event->setTitle('Write a blog post');
+		$event->setStartAt(new DateTime('tomorrow'));
+
+        $form = $this->createFormBuilder($event)
+					 ->add('title', 'text')
+					 ->add('start_at', 'date')
+					 ->add('save', 'submit')
+					 ->getForm();
+
+        return $this->render('MethinkHtmlBundle:Event:create.html.twig', array(
+            'form' => $form->createView(),
+        ));
+    }
+
+    public function createProcessAction()
+    {
+    	
     }
 }
